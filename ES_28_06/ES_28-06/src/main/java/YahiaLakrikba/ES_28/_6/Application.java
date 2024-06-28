71784c6f7a89f6ea1a6aaff5ff2f0d18b87321f7
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-06-28 13:35:58 +0200

    initial little commit

---
+++ ES_28_06/ES_28-06/src/main/java/YahiaLakrikba/ES_28/_6/Application.java
@@ -0,0 +1,13 @@
+package YahiaLakrikba.ES_28._6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ ES_28_06/ES_28-06/src/test/java/YahiaLakrikba/ES_28/_6/ApplicationTests.java
@@ -0,0 +1,13 @@
+package YahiaLakrikba.ES_28._6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
